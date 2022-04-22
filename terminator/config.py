c88e14aca0d2a133d414d04eda6cc304c894b83d
YangWanjun <ywjsailor@gmail.com>
2022-04-22 19:28:44 +0900

    database url

--- terminator/config.py
+++ terminator/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from urllib.parse import quote
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
-    'user': 'root',
+    'user': os.environ['MYSQL_ENV_MYSQL_USER'],
     'password': quote(os.environ['MYSQL_ENV_MYSQL_ROOT_PASSWORD']),
     'host': quote(os.environ['MYSQL_PORT_3306_TCP_ADDR']),
     'db': 'maintenance'
